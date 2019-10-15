7d10ee0f39a29fe92977352e2332d673f31277b8
Devin LaMarca <dhlamarca@gmail.com>
2019-10-15 11:39:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -44,23 +44,25 @@ body {
   position:absolute;
   width: auto;
   min-width: 100%;
+  margin: 0px; padding: 0px;
 }
-pre, input {
+pre {
   font-family:TopazPlus-a1200;
   font-size:16px;
   font-weight:normal;
-  color:#00b600;
-  line-height: 16px;
+  color:#ffffff;
+  line-height: 18px;
   margin: 0; padding: 0;
   margin-bottom: 1px;
 }
+
 br {
   
 }
 .g {color:#007700}
 .lg {color:#00b600}
 .w {color:#00ff00}
-.by {color:#0c1d12}
+.by {color:#0F2640}
 
 ::-moz-selection {color:000000;background:#00b600}
 .g::-moz-selection, .by::-moz-selection {color:#000000}
@@ -78,9 +80,9 @@ textarea, input {
 }
 
 .body {
-  padding:16px 8px 16px 8px;
+  padding:0px 8px 15px 5px;
   animation: textShadow 1.033s infinite;
-  max-width:440px;
+  max-width:700px;
 }
 .flicker {
   pointer-events: none;
@@ -122,6 +124,127 @@ textarea, input {
     top:99%;
   }
 }
+@keyframes scanlines {
+  0% {
+    background-position: 0 50%;
+  }
+}
+@keyframes textShadow {
+  0% {
+    text-shadow: 0.4389924193300864px 0 1px rgba(0,30,255,0.5), -0.4389924193300864px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  5% {
+    text-shadow: 2.7928974010788217px 0 1px rgba(0,30,255,0.5), -2.7928974010788217px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  10% {
+    text-shadow: 0.02956275843481219px 0 1px rgba(0,30,255,0.5), -0.02956275843481219px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  15% {
+    text-shadow: 0.40218538552878136px 0 1px rgba(0,30,255,0.5), -0.40218538552878136px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  20% {
+    text-shadow: 3.4794037899852017px 0 1px rgba(0,30,255,0.5), -3.4794037899852017px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  25% {
+    text-shadow: 1.6125630401149584px 0 1px rgba(0,30,255,0.5), -1.6125630401149584px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  30% {
+    text-shadow: 0.7015590085143956px 0 1px rgba(0,30,255,0.5), -0.7015590085143956px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  35% {
+    text-shadow: 3.896914047650351px 0 1px rgba(0,30,255,0.5), -3.896914047650351px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  40% {
+    text-shadow: 3.870905614848819px 0 1px rgba(0,30,255,0.5), -3.870905614848819px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  45% {
+    text-shadow: 2.231056963361899px 0 1px rgba(0,30,255,0.5), -2.231056963361899px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  50% {
+    text-shadow: 0.08084290417898504px 0 1px rgba(0,30,255,0.5), -0.08084290417898504px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  55% {
+    text-shadow: 2.3758461067427543px 0 1px rgba(0,30,255,0.5), -2.3758461067427543px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  60% {
+    text-shadow: 2.202193051050636px 0 1px rgba(0,30,255,0.5), -2.202193051050636px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  65% {
+    text-shadow: 2.8638780614874975px 0 1px rgba(0,30,255,0.5), -2.8638780614874975px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  70% {
+    text-shadow: 0.48874025155497314px 0 1px rgba(0,30,255,0.5), -0.48874025155497314px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  75% {
+    text-shadow: 1.8948491305757957px 0 1px rgba(0,30,255,0.5), -1.8948491305757957px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  80% {
+    text-shadow: 0.0833037308038857px 0 1px rgba(0,30,255,0.5), -0.0833037308038857px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  85% {
+    text-shadow: 0.09769827255241735px 0 1px rgba(0,30,255,0.5), -0.09769827255241735px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  90% {
+    text-shadow: 3.443339761481782px 0 1px rgba(0,30,255,0.5), -3.443339761481782px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  95% {
+    text-shadow: 2.1841838852799786px 0 1px rgba(0,30,255,0.5), -2.1841838852799786px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  100% {
+    text-shadow: 2.6208764473832513px 0 1px rgba(0,30,255,0.5), -2.6208764473832513px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+}
+@keyframes glitch-skew {
+    0% {
+        transform: skew(0deg, 0deg);
+    }
+    48% {
+        transform: skew(0deg, 0deg);
+        filter: blur(0);
+    }
+    50% {
+        transform: skew(-20deg, 0deg);
+        filter: blur(4px);
+    }
+    52% {
+        transform: skew(20deg, 0deg);
+    }
+    54% {
+        transform: skew(0deg, 0deg);
+        filter: blur(0);
+    }
+    100% {
+        transform: skew(0deg, 0deg);
+    }
+
+}
+.skull {
+  font-family:TopazPlus-a1200,monotype;
+  font-size:14px;
+  font-weight:normal;
+  margin: 0px; padding: 0px;
+  color:#00b600;
+  line-height: 14px;
+}
+
+.eye{color:#DB2727;
+    font-weight:normal;
+}
+
+.glow {
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 5px #111, 0 0 10px #ff0000, 0 0 20px #ff0000, 0 0 30px #ff0000, 0 0 40px #ff0000, 0 0 50px #ff0000, 0 0 60px #ff0000;
+  }
+  to {
+    text-shadow: 0 0 5px #111, 0 0 10px #a30303, 0 0 20px #a30303, 0 0 30px #a30303, 0 0 40px #a30303, 0 0 50px #a30303, 0 0 60px #a30303;
+  }
+}
 
     </style>
     <script  src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -131,40 +254,34 @@ textarea, input {
     
   </head>
 <body onclick="document.forms[0].cmd.focus()">
-    <?php
-        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-    ?>
+
 <div class="scanlines"></div>
 <div class="scanline"></div>
 <div class="flicker"></div>
 <div class="body">
-    <pre>
-     <span class="lg">
-        <span style="display:block;line-height:10px; font-size: 10px; font-weight:bold; font-family: monospace, monospace;">
-                           .;c:,;;;;;;;;,;;;;;,;,,.
+    <pre class='skull'>
+        <span style="lg display:block;">
+                         .;c:,;;;;;;;;,;;;;;,;,,.
                       .....,lkxloooloooolooooolol:,.....
                      ..:l:::loolooololodldoooolooooolll..
                  .';;,;locccllllooololooldoddoloooddooo;,,,';.
                ...;xdoddolcclllloolclclololddoloooddooddool,o:.
                :lcldolodolllolcclllccclololdoollllooooooooolllc'.
-           ..,,cololcclllccclc;:ccl:c:clcllooccc:clollolccc:cc:''''
-          .::cllololcccllccclc;;::c:l::lloloolcc;clolloc:;;;;:;:lc;...
-        .cc::::loloc:;:::cc:ccc:cc::l;,cloloooc:::col::,'',,,::clc:::...
+           ..,,cololcclllccclc;:ccl:c:clcllcooccc:clollolccc:clc;...
+         .cc::::loloc:;:::cc:ccc:c::l;,cloloooc:::col::,'',,,::clc:::.
        ',ll::c:llclc:;;;;;:;::::c:;:l:;::c:ccccc:cclc::::ccclolllllll,'.
        ldolllccclclc:;;;;;:;;;::c:::l:;;;:::cccolloolldolooodolllolllll.
      .;dddoloc:clll;',;;;:;'',;:lcccc,':lc;:cccoolol;,oo:;:xdolloooooll,'.
      ,cdddoloc:,,',...........,llcc,...',.'cclcll:;'..'.. .:;;;looooooo::.
-    .loooollll:.             .;oc:c.      .lllc;l:.          ..:ooooooolc.
-   ',lllllllc'.               .,ccl.      .ccl; ..             ..,lxdloc:;;.
-   .,lllllc;,.                 'ccl,       ';l,                  .;xdoolc::'..
-   .'cccll:                    ':;c;.      .lx'                    cololc,',,..
-  .,;::'':;                  ..,c...        ,dc:,                  :o,:dc;;...
-  .:::c::c;              ....',;c'          .:ld;.'                ;c,:olcl,.   
-  .;;;:c:c:             .:c;;;,;c'.          .d0::x,               ;lclooll..
-  .,,;:::c:        ..,,,,;:,,,,..             'dccdo:::;,',::.     ;lclolll,..
- ..'';;::c:......',;cl:cc::,,,,.       ...    .lcloollolccodo,'....:ccllccl:;.
+    .loooollll:.<span class="eye glow">0110010101111</span>.;oc:c.      .lllc;l:.<span class="eye glow">0101111001</span>..:ooooooolc.
+   ',lllllllc'.<span class="eye glow">001011001010111</span>.,ccl.      .ccl;,..<span class="eye glow">0111001100100</span>..,lxdloc:;;.
+   .,lllllc;,.<span class="eye glow">00110010000001100</span>'ccl,       ';l,<span class="eye glow">000011111100010010</span>.;xdoolc::'..
+   .'cccll:<span class="eye glow">11001110101011011000</span>':;c;.      .lx'<span class="eye glow">00110001001101001011</span>.ololc,',,..
+  .,;::'':;<span class="eye glow">110110000100000011</span>..,c...        ,dc:,<span class="eye glow">011100110000101110</span>.o,:dc;;...
+  .:::c::c;<span class="eye glow">01111011001100</span>....',;c'          .:ld;..<span class="eye glow">0100111100101101</span>.c,:olcl,.   
+  .;;;:c:c:<span class="eye glow">0100000011010</span>.:c;;;,;c'.          .d0::x,<span class="eye glow">011011001010111</span>.lclooll..
+  .,,;:::c:<span class="eye glow">11011001</span>..,,,,;:,,,,..             'dccdo:::;,',::.<span class="eye glow">1001</span>.lclolll,..
+ ..'';;::c:......,,;cl:cc::,,,,.       ...    .lcloollolccodo,'....:ccllccl:;.
 .;;'',,cccccc:;:::cccllllll:;,;.      ;c;.    .olloollooloooooooooollcl:,:cc:.
 .;;,;c;;:c:cc:;:::cccloc::cllcc;;'',;;ll,;,,,;:olodoloooloooddoodollccl:;:c::.
 .;;,:o:,;:;:c:;::::cccc:;;;clc:cl:;:lccl:lccllclcloollooooolooololccccl;,::cc..
@@ -173,18 +290,16 @@ textarea, input {
 .,,',::.  ..          .:;c:clllllcllclcllolllllccoddoc;..        .. ...',coll...
 .,,,,::,:,.          :;clllocc:::ccc:::clclcloolllooccoodl.           .:dlcol:.
 .,;;,;;,:l,....      .'clcl::,,;::c;,,:c:lclllcccloolll,'      .''''',cllllc:..
-.,,;;:::clcccc;      .'clcl;,,,;:cl::;:c:lc:clc::loolol''.     ;lolllllllcc:,..
-  .;,,,:cc:lc,;.     coolcc:;,,;:cc:::ccclccc::ccloolllll.     ;:lollcl:;;;..
   ...',::c:::;;...   :lllcc;;,';:;,,,;:;,;,,:;::lolcllol......clccclcc;;,....
      ',;:cc::c:,c' .'loollloooo;;@@@@@@@@@@@@@;llool':;..;oo::cccccc.
      ',;::::ccccl;..'ddolll;:c;@@@@@@@@@@@@@@@@@;llooc... .cd;,,;;;:cc.
-     .',,;;:ccc:l:..,ldolo:;,;@@@@@@        @@@@@@;,:odc',. .ld;;;:ccl::..
-     .',',,;::::l:',;co;;;;;;@@@@@,           @@@@@;;;;;o; .okooololo';:..
+     .',,;;:ccc:l:..,ldolo:;,;@@@@@@       @@@@@@;,:odc',. .ld;;;:ccl::..
+     .',',,;::::l:',;co;;;;;;@@@@@          @@@@@@;;;;;o; .okooololo';:..
        ...,::::::. ..':;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@;;... :olool,..'...
          ..ccc:c:    .,;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@;'.   :cldoo'....
            ;:c:::      ;@@@@@@@@@@@@@@@@@@@@@@@###@@@@@;     cllooo...
          ..;::::;      ;@@@@@                ######@@@@;     colooo,.. 
-         ..:::;,'      ;@@@@@               #####*@@@@@;    .ldoddo,,'.
+         ..:::;,'      ;@@@@@  <span class="by" style="font-size:18px;">~$ help</span>    #####*@@@@@;    .ldoddo,,'.
          :lc:;,,,.,.   ;@@@@@             ######  @@@@@;  ':dooddoc:..
          ;:;;;;;::c.   ;@@@@@    ###     #####    @@@@@; .:ldooddoll.
          ..;:;,::cc'.  ;@@@@@  ####### ######     @@@@@;.'ooollooo::.
@@ -197,7 +312,6 @@ textarea, input {
                 .colccc;=;=;=;=;=;=;=;=;=;=;=;=;=;=;=;=;::doodc:..
                  oloooooccloooloooddoodollcclccclcooolloolllclo..
                 ;:lolllooolo;cl. ....:,,;','...  .,;lc::;;:ll':,.
-                 .colcccooclcldl:ooololooclcl:olccccllclccclo...
                   .,:::cllclloolcoooododdldc:collolllolllc:;',.
                     ,;cloolooddooddoododdldlc;ccllcloolll:;;,..
                      ...ldooooddoolc:cclc,;:lloc:cooolll.....
@@ -208,18 +322,39 @@ textarea, input {
                             ';cooddoododdldlclolocc:.
                             ..:oldoooolodldoolccc,...
                               ';,:::;:;::;:;::;;,..
-    </span>
-     </span> 
-    </pre>
+</span> 
+</pre>
 <br/>
+<div id="loading-message" class="lg">
+<?php
+    $cookie_name = "slctf";
+    $cookie_value = "sitelock";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    setcookie("cookie_key","c2xjdGZbeHh4eHh4XQ==", time() + (86400 * 30), "/");
+    
+    if(!isset($_COOKIE[$cookie_name])) {
+        echo "Welcome";
+        
+    } else {
+        echo "Welcome Back.";
+    }
+?>
+</div>
 <div class="w">
     <form id="bash">
-    ~$ <input id="cmd" type="text" autocomplete="off" />
+    ~$ <input id="cmd" class="w" type="text" autocomplete="off" size="75" maxlength="75"/>
     <input id="submit" type="submit" style="display: none;"/></form>
 </div>
-<div id="display" class="lg"></div></br></br>
+<div id="display" class="w"></div></br></br>
 </div>
-
+<script>
+  setTimeout(function(){
+    /*document.getElementById('loading-message').style.display = 'none';*/
+    var item = document.getElementById('loading-message')
+    item.parentNode.document.innerHTML = ""; 
+    
+  }, 3000);
+</script>
 <script>
 $(document).ready(function() {
     $("#bash").submit(function(event) {
